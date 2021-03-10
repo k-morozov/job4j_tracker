b436d4dd449c42679ec3a0648a19444aad899202
Konstantin Morozov <konstantin.morozov@openmedia.co>
2021-03-10 07:11:54 +0300

    7. Date. Отображение даты. [#488994]

---
+++ src/main/java/ru/job4j/tracker/UsagePackageDate.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class UsagePackageDate {
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Текущая дата: " + currentDate);
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time: " + currentTime);
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm::ss");
+        String currentStrDateTime = currentDateTime.format(formater);
+        System.out.println("current: " + currentStrDateTime);
+    }
+}
